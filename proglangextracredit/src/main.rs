e6aa337d0842deead6dd33eaf057c62437ff4d20
spencers1 <spencer.sean22@gmail.com>
2018-04-14 17:28:47 -0400

    Update main.rs

--- proglangextracredit/src/main.rs
+++ proglangextracredit/src/main.rs
@@ -14,10 +14,20 @@ fn main() {
     f.read_to_string(&mut contents)         // reads the file to a string
         .expect("something went wrong reading the file");
 
-    let mut numbers: Vec<usize> =       // converts string into vector of ints
-        contents.split_whitespace()
-                .map(|s| s.parse().unwrap())
-                .collect();
+    let mut numbers = convert_to_vector(&mut contents);
+
+    if numbers[0] > 0 {
+    calculate_meals(&mut numbers);
+    } else {
+        println!("Unsatisfactory Meal");
+    }
+
+}
+
+fn calculate_meals(numbers: &mut Vec<usize>) {
+    if numbers[0] == 0 {
+        println!("Unsatisfactory Meal");
+    }
 
     let mut meatcounter: usize = 0;    // made a counter for each type of dish
     let mut vegcounter = 0;
@@ -26,11 +36,11 @@ fn main() {
     let mut dairycounter = 0;
     let mut peanutscounter = 0;
     let mut i: usize = 1;
-    let size: usize = numbers[0] * 7
-    println!("total dishes {}", totaldishes);
+    let size: usize = numbers[0] * 7;
     let mut done = false;
     numbers.push(0);      // I kept getting an index out of bounds error
     numbers.push(0);      // so I added a couple more idk if this is frowned upon
+
     while !done {
 
         meatcounter += numbers[i+1];
@@ -45,29 +55,26 @@ fn main() {
         } else {
             done = false;
         }
-
     }
+        let numdishes = numbers[0];
 
-    println!("meatcounter {}", meatcounter); // just printed them to make sure it worked
-    println!("vegcounter {}", vegcounter);
-    println!("orgcounter {}", orgcounter);
-    println!("dessertcounter {}", dessertcounter);
-    println!("dairycounter {}", dairycounter);
-    println!("peanutscounter {}", peanutscounter);
-    println!("number of dishes {}", numbers[0]);
-
-    let numdishes = numbers[0];
-
-    if numdishes - meatcounter > 2 && numdishes - vegcounter > 2 && // check if each one satisfies
-         orgcounter > 2 && numdishes - dessertcounter > 2 &&
-        numdishes - dairycounter > 2 && numdishes - peanutscounter > 2 {
-            println!("Satisfactory Meal");
-        } else {
-            println!("Unsatisfactory Meal");
-        }
-
+        if numdishes - meatcounter > 2 && numdishes - vegcounter > 2 && // check if each one satisfies
+             orgcounter > 2 && numdishes - dessertcounter > 2 &&
+            numdishes - dairycounter > 2 && numdishes - peanutscounter > 2 {
+                println!("Satisfactory Meal");
+            } else {
+                println!("Unsatisfactory Meal");
+            }
 }
 
+
+fn convert_to_vector(contents: &str) -> Vec<usize> {
+    let numbers: Vec<usize> =       // converts string into vector of ints
+        contents.split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect();
+    (numbers)
+}
 
 fn parser(args: &[String]) -> (&str) {
     let filename = &args[1];
